8d02b549e534c936707a6fbb1955e05ebdcdbf90
Saad-Ali88 <saadalise72@gmail.com>
2026-05-08 23:57:57 +0500

    Add Testimonial section

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-
 const menuOpenButton = document.querySelector("#menu-open-button");
-
 const menuCloseButton = document.querySelector("#menu-close-button");
 
-menuOpenButton.addEventListener("click",() => {
+menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
+});
 
-    menuCloseButton.addEventListener("click", () => menuCloseButton.click())
+// Fix: was calling menuCloseButton.click() instead of removing the class
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+// Fix: selector changed from '.slider-swiper' to '.slider-container'
+const swiper = new Swiper('.slider-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        786:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+    }
 });
